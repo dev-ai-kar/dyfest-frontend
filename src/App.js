b7e21529a32537583f50b2257fe362ec86b0d6af
Vaibhav Deokar <deokar.vaibhav@outlook.com>
2021-02-05 19:31:43 +0530

    Material UI Setup, Header and Footer Components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,60 @@
-function App() {
-  return <div>My App</div>;
+import "assets/scss/material-kit-react.scss";
+import "./App.css";
+// nodejs library that concatenates classes
+import classNames from "classnames";
+// @material-ui/core components
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+// core components
+import Header from "components/Header/Header.js";
+import Footer from "components/Footer/Footer.js";
+
+import HeaderLinks from "components/Header/HeaderLinks.js";
+
+import styles from "assets/jss/material-kit-react/views/components.js";
+
+const useStyles = makeStyles(styles);
+
+function App(props) {
+  const classes = useStyles();
+  const { ...rest } = props;
+  return (
+    <div>
+      <Header
+        brand="DY FEST"
+        rightLinks={<HeaderLinks />}
+        fixed
+        color="transparent"
+        changeColorOnScroll={{
+          height: 400,
+          color: "dark",
+        }}
+        {...rest}
+      />
+      <header className="App-header">
+        <Button
+          style={{ fontSize: 25 }}
+          size="large"
+          variant="contained"
+          color="primary"
+        >
+          Hello World
+        </Button>
+      </header>
+      <div className={classNames(classes.main, classes.mainRaised)}>
+        <main>
+          <br />
+          <h1>Welcome</h1>
+          <h1>to The</h1>
+          <h1>other side</h1>
+          <br />
+        </main>
+      </div>
+      <br />
+      <h1>Lets Do It</h1>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Header/HeaderLinks.js
@@ -0,0 +1,102 @@
+/*eslint-disable*/
+import React from "react";
+import DeleteIcon from "@material-ui/icons/Delete";
+import IconButton from "@material-ui/core/IconButton";
+// react components for routing our app without refresh
+// import { Link } from "react-router-dom";
+
+// @material-ui/core components
+import { makeStyles } from "@material-ui/core/styles";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import Tooltip from "@material-ui/core/Tooltip";
+
+// @material-ui/icons
+import { Apps, AccountBox, ShoppingCart } from "@material-ui/icons";
+
+// core components
+import CustomDropdown from "components/CustomDropdown/CustomDropdown.js";
+import Button from "components/CustomButtons/Button.js";
+
+import styles from "assets/jss/material-kit-react/components/headerLinksStyle.js";
+
+const useStyles = makeStyles(styles);
+
+export default function HeaderLinks(props) {
+  const classes = useStyles();
+  return (
+    <List className={classes.list}>
+      <ListItem className={classes.listItem}>
+        <Button
+          href="#"
+          color="transparent"
+          target="_blank"
+          className={classes.navLink}
+        >
+          <AccountBox className={classes.icons} /> Login
+        </Button>
+      </ListItem>
+      <ListItem className={classes.listItem}>
+        <Button
+          href="#"
+          color="transparent"
+          target="_blank"
+          className={classes.navLink}
+        >
+          <ShoppingCart className={classes.icons} /> Cart
+        </Button>
+      </ListItem>
+      <ListItem className={classes.listItem}>
+        <Tooltip
+          id="instagram-twitter"
+          title="Subsctibe us on youtube"
+          placement={window.innerWidth > 959 ? "top" : "left"}
+          classes={{ tooltip: classes.tooltip }}
+        >
+          <Button
+            href="https://www.youtube.com/kalaraag"
+            target="_blank"
+            color="transparent"
+            className={classes.navLink}
+          >
+            <i className={classes.socialIcons + " fab fa-youtube"} />
+          </Button>
+        </Tooltip>
+      </ListItem>
+      <ListItem className={classes.listItem}>
+        <Tooltip
+          id="instagram-facebook"
+          title="Follow us on facebook"
+          placement={window.innerWidth > 959 ? "top" : "left"}
+          classes={{ tooltip: classes.tooltip }}
+        >
+          <Button
+            color="transparent"
+            href="https://www.facebook.com/Kalaraag/"
+            target="_blank"
+            className={classes.navLink}
+          >
+            <i className={classes.socialIcons + " fab fa-facebook"} />
+          </Button>
+        </Tooltip>
+      </ListItem>
+      <ListItem className={classes.listItem}>
+        <Tooltip
+          id="instagram-tooltip"
+          title="Follow us on instagram"
+          placement={window.innerWidth > 959 ? "top" : "left"}
+          classes={{ tooltip: classes.tooltip }}
+        >
+          <Button
+            color="transparent"
+            href="https://www.instagram.com/kalaraag/"
+            target="_blank"
+            className={classes.navLink}
+          >
+            <i className={classes.socialIcons + " fab fa-instagram"} />
+          </Button>
+        </Tooltip>
+      </ListItem>
+    </List>
+  );
+}
