ee0b48ac9e66385477530f171910517328ccedac
Vaibhav Deokar <deokar.vaibhav@outlook.com>
2021-02-09 19:26:12 +0530

    Product List Reducer & Action

--- src/store.js
+++ src/store.js
@@ -1,8 +1,11 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
+import { eventListReducer } from "./reducers/eventsReducers";
 
-const reducer = combineReducers({});
+const reducer = combineReducers({
+  eventList: eventListReducer,
+});
 
 const initialState = {};
 
